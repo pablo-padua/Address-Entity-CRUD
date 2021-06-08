7ce358f95924df6242147e444d9b6f604f8268d1
Pablo Padua <pablopaduav@gmail.com>
2021-06-08 01:00:52 -0300

    Att endpoints

--- src/main/java/com/entity/Entity/AddressResource.java
+++ src/main/java/com/entity/Entity/AddressResource.java
@@ -3,11 +3,13 @@ package com.entity.Entity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -33,7 +35,32 @@ public class AddressResource {
     **/
     @PostMapping("/create")
     public ResponseEntity<AddressEntity> createNewAddress(){
+        return new ResponseEntity<>(addressService.createNewAddress(), HttpStatus.CREATED);
+    }
+
+    /**
+     * Post request to create a new hardcoded address
+     **/
+    @PostMapping("/create-hardcoded")
+    public ResponseEntity<AddressEntity> createHardcodedAddress(){
         AddressEntity result = addressService.createHardcodedAddress();
         return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
+
+    /**
+     * Put request to update an address' number
+     **/
+    @PutMapping("/update-number/{id}")
+    public ResponseEntity<Optional<AddressEntity>> updateAddressNumber(@PathVariable Long id, @RequestParam Long number) {
+        return ResponseEntity.ok(addressService.updateAddressNumber(id, number));
+    }
+
+    /**
+     * Delete request to delete an address entity based on its ID
+     **/
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable Long id){
+        return new ResponseEntity<>(addressService.deleteById(id), HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/entity/Entity/AddressService.java
+++ src/main/java/com/entity/Entity/AddressService.java
@@ -1,53 +1,92 @@
 package com.entity.Entity;
 
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.swing.text.MaskFormatter;
+import java.text.ParseException;
 import java.util.Optional;
 
 @Service
 @Transactional
 public class AddressService {
 
-    private final AddressRepository addressRepository;
+    @Autowired
+    private AddressRepository addressRepository;
 
-    public AddressService(AddressRepository addressRepository) {
-        this.addressRepository = addressRepository;
-    }
-
-    public void getRandomAddressBasedOnCEP(AddressEntity addressEntity){
-        URL url = new URL("http://geradornv.com.br/wp-json/api/cep/random-by-states");
-        HttpURLConnection httpURLConnection = url.openConnection();
-        url.setRequestMethod("GET");
-        int responseCode = url.getResponseCode();
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            HttpResponse<JsonNode> response = Unirest.get(url)
-                    .queryString("state","SP")
-                    .asString();
-            //Results in random CEP searches based on Sao Paulo state
+    public void getRandomAddressBasedOnCEP(AddressEntity addressEntity) {
+        try {
+            MaskFormatter cepMask = new MaskFormatter("#####-###");
+            cepMask.setValueContainsLiteralCharacters(false);
+            //Results in random CEP searches based in Sao Paulo state
+            HttpResponse<JsonNode> response = Unirest.get("http://geradornv.com.br/wp-json/api/cep/random-by-states")
+                    .queryString("state", "SP")
+                    .asJson();
+            JSONObject jsonObject = response.getBody().getObject();
+            addressEntity.setStreetName(jsonObject.getString("type") + " " + jsonObject.getString("street"));
+            addressEntity.setNumber(0L); //This api doesn't support address number :(
+            addressEntity.setNeighbourhood(jsonObject.getString("neighborhood"));
+            addressEntity.setCity(jsonObject.getString("city"));
+            addressEntity.setState(jsonObject.getString("state"));
+            addressEntity.setZipCode(cepMask.valueToString(jsonObject.getString("cep")));
+            addressEntity.setCountry("Brazil");
+        } catch (ParseException e) {
+            //Error masking zipCode(CEP) to the #####-### pattern
         }
-        url.disconnect();
     }
 
-    public void createNewAddress(){
+    public void getLatLongGoogleApi(AddressEntity addressEntity) {
+        //Format string for the maps.google api and search for address' latitude and longitude
+        String address = addressEntity.getStreetName() + " " +
+                addressEntity.getNeighbourhood() + " " +
+                addressEntity.getCity() + " " +
+                addressEntity.getState() + " " +
+                addressEntity.getCountry();
+        String key = "AIzaSyCj0cY2yEvVfYhAaTz3-P2MW-YRKmhz5Uw";
+        HttpResponse<JsonNode> response = Unirest.get("https://maps.googleapis.com/maps/api/geocode/json")
+                .queryString("address", address)
+                .queryString("key", key)
+                .asJson();
+        JSONArray jsonArray = response.getBody().getObject().getJSONArray("results");
+        String lat = (jsonArray.getJSONObject(0).getJSONObject("geometry").getJSONObject("location").getString("lat"));
+        String lng = (jsonArray.getJSONObject(0).getJSONObject("geometry").getJSONObject("location").getString("lng"));
+        addressEntity.setLatitude(Double.parseDouble(lat));
+        addressEntity.setLongitude(Double.parseDouble(lng));
+    }
+
+    public AddressEntity createNewAddress() {
         AddressEntity addressEntity = new AddressEntity();
         getRandomAddressBasedOnCEP(addressEntity);
-        if (addressEntity.getLatitude() == null){
-
+        if (addressEntity.getLatitude() == null || addressEntity.getLongitude() == null) {
+            getLatLongGoogleApi(addressEntity);
         }
-        if (addressEntity.getLongitude() == null){
-
-        }
-        addressRepository.save(addressEntity);
-    }
-
-    public AddressEntity createHardcodedAddress(){
-        AddressEntity addressEntity = new AddressEntity("Rua Das Alamedas", 11L,"Bairro Sao Joao", "Sao Paulo", "Estado de Sao Paulo", "Brazil", "VS1LC");
         return addressRepository.save(addressEntity);
     }
 
-    public Optional<AddressEntity> getAddressById(Long id){
+    public AddressEntity createHardcodedAddress() {
+        AddressEntity addressEntity = new AddressEntity("Rua Das Alamedas", 11L, "Bairro Sao Joao", "Sao Paulo", "Estado de Sao Paulo", "Brazil", "VS1LC");
+        return addressRepository.save(addressEntity);
+    }
+
+    public Optional<AddressEntity> getAddressById(Long id) {
         return addressRepository.findById(id);
+    }
+
+    public Optional<AddressEntity> updateAddressNumber(Long id, Long number) {
+        Optional<AddressEntity> addressEntity = addressRepository.findById(id);
+        addressEntity.ifPresent(entity -> entity.setNumber(number));
+        return addressEntity;
+    }
+
+    public String deleteById(Long id) {
+        addressRepository.deleteById(id);
+        return String.format("Address with id: %s was deleted", id);
     }
 
 }
